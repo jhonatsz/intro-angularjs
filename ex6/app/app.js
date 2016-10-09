108c4f63849c6ddc89f995468494134e7c9433be
kaiifalcutela <lcaii.sann@gmail.com>
2016-10-09 11:41:37 +0800

    - adding routes

---
+++ ex6/app/app.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    myModule = angular.module('myAngularApplication', ['ngRoute'])
+        .config(['$routeProvider', function ($routeProvider) {
+
+            $routeProvider
+            .when("/home", {
+                templateUrl: "home.html",
+                controller: "homeController"
+            })
+            .when("/about", {
+                templateUrl: "about.html",
+                controller: "aboutController"
+            })
+            .when("/contact", {
+                templateUrl: "contact.html",
+                controller: "contactController"
+            }).
+            otherwise({
+                redirect: '/home'
+            });
+
+          
+        }]);
+}());
